1db2d6a38f640e01376e3f4fa46936a4029f054c
Ethan Buck <ST10250745@vcconnect.edu.za>
2024-10-18 08:47:11 +0200

    Data

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PROGPART1.Models; 
+
+namespace PROGPART1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Claim> Claims { get; set; }
+        public DbSet<Approval> Approvals { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>().ToTable("Users");
+            modelBuilder.Entity<Claim>().ToTable("Claims");
+            modelBuilder.Entity<Approval>().ToTable("Approvals");
+
+            modelBuilder.Entity<User>()
+                .HasKey(u => u.UserId);
+
+            modelBuilder.Entity<Claim>()
+                .HasKey(c => c.ClaimId);
+
+            modelBuilder.Entity<Approval>()
+                .HasKey(a => a.ApprovalId);
+        }
+    }
+}
